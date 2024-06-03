8b49a132c7cce6b3f80fa48ba1466fa16e8a5d82
Nigel Horne <njh@bandsman.co.uk>
2024-06-03 13:37:38 -0400

    Release 0.04

--- lib/Lingua/String.pm
+++ lib/Lingua/String.pm
@@ -13,11 +13,11 @@ Lingua::String - Class to contain a string in many different languages
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use overload (
         # '==' => \&equal,
