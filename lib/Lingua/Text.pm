dbc8bf4036d3931db577ad263f67317cfb09c1cf
Nigel Horne <njh@bandsman.co.uk>
2026-08-19 09:27:09 -0400

    comments

--- lib/Lingua/Text.pm
+++ lib/Lingua/Text.pm
@@ -2,12 +2,13 @@ package Lingua::Text;
 
 use strict;
 use warnings;
+use autodie qw(:all);
 
 use Carp;
 use HTML::Entities;
 use Object::Configure;
 use Params::Get;
-use Scalar::Util;
+use Scalar::Util qw(blessed);
 use I18N::LangTags::Detect;
 
 # TODO: Investigate Locale::Maketext
@@ -23,13 +24,12 @@ Version 0.08
 =cut
 
 our $VERSION = '0.08';
+our $AUTOLOAD;
 
 use overload (
-	# '==' => \&equal,
-	# '!=' => \&not_equal,
 	'""' => \&as_string,
 	bool => sub { 1 },
-	fallback => 1   # So that boolean tests don't cause as_string to be called
+	fallback => 1	# So that boolean tests don't cause as_string to be called
 );
 
 =head1 SYNOPSIS
@@ -74,24 +74,23 @@ Uses Carp::carp to log warnings for incorrect usage or potential mistakes.
 sub new {
 	my $class = shift;
 
-	# Handle hash or hashref arguments
 	my $params;
 	if((scalar(@_) == 1) && (!ref($_[0])) && (my $lang = _get_language())) {
-		$params->{$lang} = $_[0];
-	} else {
+		$params = { $lang => $_[0] };
+	} elsif(@_) {
 		$params = Params::Get::get_params(undef, @_);
 	}
 
 	if(!defined($class)) {
-		if((scalar keys %{$params}) > 0) {
+		if($params && scalar keys %{$params}) {
 			# Using Lingua::Text->new(), not Lingua::Text::new()
-			carp(__PACKAGE__, ' use ->new() not ::new() to instantiate');
+			Carp::carp(__PACKAGE__, ' use ->new() not ::new() to instantiate');
 			return;
 		}
 
 		# FIXME: this only works when no arguments are given
 		$class = __PACKAGE__;
-	} elsif(Scalar::Util::blessed($class)) {
+	} elsif(blessed($class)) {
 		# If $class is an object, clone it with new arguments
 		if($params) {
 			return bless { texts => {%{$class->{'texts'}}, %{$params}} }, ref($class);
@@ -100,7 +99,6 @@ sub new {
 	}
 
 	$params = Object::Configure::configure($class, $params);
-	# Return the blessed object
 	if($params) {
 		return bless { texts => $params }, $class;
 	}
@@ -120,8 +118,7 @@ Autoload will do this for you as
 
 =cut
 
-sub set
-{
+sub set {
 	my $self = shift;
 
 	if(!@_) {
@@ -150,10 +147,9 @@ sub set
 
 # https://www.gnu.org/software/gettext/manual/html_node/Locale-Environment-Variables.html
 # https://www.gnu.org/software/gettext/manual/html_node/The-LANGUAGE-variable.html
-sub _get_language
-{
+sub _get_language {
 	for my $tag (I18N::LangTags::Detect::detect()) {
-		if ($tag =~ /^([a-z]{2})/i) {
+		if($tag =~ /^([a-z]{2})/i) {
 			return lc($1);
 		}
 	}
@@ -161,20 +157,17 @@ sub _get_language
 		return lc($1);
 	}
 
-	foreach my $variable('LC_ALL', 'LC_MESSAGES', 'LANG') {
+	for my $variable('LC_ALL', 'LC_MESSAGES', 'LANG') {
 		my $val = $ENV{$variable};
-		next unless(defined($val));
+		next unless defined($val);
 
 		if($val =~ /^([a-z]{2})/i) {
 			$val = lc($1);
-			return lc($val) if _is_valid_language($val);
+			return $val if _is_valid_language($val);
 		}
 	}
 
-	# if(defined($ENV{'LANG'}) && (($ENV{'LANG'} =~ /^C\./) || ($ENV{'LANG'} eq 'C'))) {
-		# return 'en';
-	# }
-	return 'en' if defined $ENV{'LANG'} && $ENV{'LANG'} =~ /^C(\.|$)/;
+	return 'en' if defined($ENV{'LANG'}) && $ENV{'LANG'} =~ /^C(\.|$)/;
 	return;	# undef
 }
 
@@ -192,22 +185,19 @@ If that parameter is not given, the system language is used.
 
 sub as_string {
 	my $self = shift;
-	my %params;
 
-	if(ref($_[0]) eq 'HASH') {
-		%params = %{$_[0]};
-	} elsif((scalar(@_) % 2) == 0) {
-		if(defined($_[0])) {
-			%params = @_;
-		}
-	} else {
-		$params{'lang'} = shift;
+	my $lang;
+	if(@_ && defined($_[0])) {
+		my $params = Params::Get::get_params('lang', @_);
+		$lang = $params && $params->{'lang'};
 	}
+	$lang ||= $self->_get_language();
 
-	if(my $lang = ($params{'lang'} || $self->_get_language())) {
+	if($lang) {
 		return $self->{'texts'}->{$lang};
 	}
 	Carp::carp(__PACKAGE__, ': usage: as_string(lang => $language)');
+	return;
 }
 
 =head2 encode
@@ -216,7 +206,7 @@ sub as_string {
 
 Turns the encapsulated texts into HTML entities
 
-    my $text = Lingua::Text->new(en => 'study', fr => 'étude')->encode();
+    my $text = Lingua::Text->new(en => 'study', fr => 'etude')->encode();
     print $text->fr(), "\n";	# Prints &eacute;tude
 
 =cut
@@ -224,45 +214,35 @@ Turns the encapsulated texts into HTML entities
 sub encode {
 	my $self = shift;
 
-	while(my($k, $v) = each(%{$self->{'texts'}})) {
-		utf8::decode($v) unless utf8::is_utf8($v);  # Only decode if not already UTF-8
-		$self->{'texts'}->{$k} = HTML::Entities::encode_entities($v);
+	for my $lang (keys %{$self->{'texts'}}) {
+		my $v = $self->{'texts'}->{$lang};
+		utf8::decode($v) unless utf8::is_utf8($v);
+		$self->{'texts'}->{$lang} = HTML::Entities::encode_entities($v);
 	}
 	return $self;
 }
 
-sub AUTOLOAD
-{
-	our $AUTOLOAD;
+sub AUTOLOAD {
 	my $self = shift or return;
 
-	# Extract the key name from the AUTOLOAD variable
 	my ($key) = $AUTOLOAD =~ /::(\w+)$/;
 
-	# Skip if called on destruction
 	return if $key eq 'DESTROY';
-
-	# Ensure the key is called on the correct package object
 	return unless ref($self) eq __PACKAGE__;
-
-	# Only allow 2-letter language codes
 	return unless $key =~ /^[a-z]{2}$/i;
-
 	return unless _is_valid_language($key);
 
 	if(my $value = shift) {
-		# Set the requested language ($key) to the given text ($value)
 		$self->{'texts'}->{$key} = $value;
 	}
 
-	# Get the requested language ($key)
 	return $self->{'texts'}->{$key};
 }
 
 # Language validation
 sub _is_valid_language {
 	my $lang = shift;
-	return $lang =~ /^[a-z]{2}(?:_[A-Z]{2})?$/;  # en or en_US format
+	return $lang =~ /^[a-z]{2}(?:_[A-Z]{2})?$/;	# en or en_US format
 }
 
 =head1 AUTHOR
